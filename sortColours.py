793a7770e2083eeb857d9d7f672144e3ee6dae41
harish4459 <harish.sunny9@gmail.com>
2022-07-11 09:42:43 +0530

    Update sortColours.py

--- sortColours.py
+++ sortColours.py
@@ -35,16 +35,15 @@ class Solution:
         
         """
         left = 0
-        right = len(nums - 1)
-        
+        right = len(nums) -1
+        i = 0
         while i <= right:
             if (nums[i] == 0):
-                nums[i], nums[left] = numes[left], nums [i]
+                nums[i], nums[left] = nums[left], nums [i]
                 left += 1
                 i += 1
             elif (nums[i] == 2):
-                nums[i], nums[right] = numes[right], nums [i]
+                nums[i], nums[right] = nums[right], nums [i]
                 right -= 1
             else:
                 i += 1
-                
